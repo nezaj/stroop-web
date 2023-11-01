de28a6fe31aff4a01a736599441859c636ee5098
Joe Averbukh <joeaverbukh@gmail.com>
2023-11-01 13:15:02 -0400

    Update landing page

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import "./index.css";
 // Styles
 // -----------------------------
 const bgColor = "bg-violet-600";
-const infoTextColor = "text-yellow-400";
-
 const colorStyleMap = {
   "text-red-400": { color: "rgb(248 113 113)" },
   "text-green-400": { color: "rgb(74 222 128)" },
   "text-blue-400": { color: "rgb(96 165 250)" },
   "text-yellow-400": { color: "rgb(250 204 21)" },
 };
-
 const infoTextStyle =
-  "text-xl my-4 text-slate-100 font-semibold text-left leading-8";
+  "justify-start text-lg my-4 text-slate-100 font-medium text-left leading-8";
+const linkTextColor = "text-yellow-400";
+const linkTextStyle = `text-center font-semibold underline ${linkTextColor}`;
 
 // avatarColor
 // -----------------------------
@@ -146,9 +145,39 @@ function Race({ players, points, goal = MULTIPLAYER_SCORE_TO_WIN }) {
   );
 }
 
-// Main
+// Shared Components
 // -----------------------------
-function App() {
+function Logo() {
+  return (
+    <div className="my-4">
+      <img
+        src="./icon.png"
+        className="w-[250px] mx-auto border-yellow-400 border-2 rounded-xl my-4"
+      />
+      <div className="justify-end text-5xl font-bold text-yellow-400">
+        Stroopwafel
+      </div>
+    </div>
+  );
+}
+
+function Badge() {
+  return (
+    <div className="flex justify-center my-4">
+      <a href="https://apps.apple.com/us/app/stroopwafel/id6470153525">
+        <img
+          src="/badge.svg"
+          alt="Download on the App Store"
+          className="w-[150px]"
+        />
+      </a>
+    </div>
+  );
+}
+
+// Pages
+// -----------------------------
+function HowToPlay({ setPageKey }) {
   const [score, setScore] = useState(0);
   const [label, setLabel] = useState(chooseRandomColor());
   const [color, setColor] = useState(chooseRandomColor());
@@ -166,24 +195,24 @@ function App() {
   const Grid = ({ onClick }) => (
     <div className="flex flex-wrap justify-center my-4 mx-8">
       <div>
-        <div
+        <button
           onClick={() => onClick("red")}
           className="w-28 h-28 bg-red-400 m-1 hover:cursor-pointer"
-        ></div>
-        <div
+        ></button>
+        <button
           onClick={() => onClick("green")}
           className="w-28 h-28 bg-green-400 m-1 hover:cursor-pointer"
-        ></div>
+        ></button>
       </div>
       <div>
-        <div
+        <button
           onClick={() => onClick("blue")}
           className="w-28 h-28 bg-blue-400 m-1 hover:cursor-pointer"
-        ></div>
-        <div
+        ></button>
+        <button
           onClick={() => onClick("yellow")}
           className="w-28 h-28 bg-yellow-400 m-1 hover:cursor-pointer"
-        ></div>
+        ></button>
       </div>
     </div>
   );
@@ -191,87 +220,171 @@ function App() {
   const colorKey = `text-${color}-400`;
   const labelColor = `text-${label}-400`;
   return (
-    <div className={`${bgColor} py-8`}>
-      <div className={`flex-col text-center w-[${SCREEN_WIDTH}px] mx-auto`}>
-        <div className="flex flex-wrap space-x-2 items-center justify-center my-4">
-          {stroops.map(([label, color], i) => (
-            <Stroop key={i} label={label} color={color} />
-          ))}
-        </div>
-        <span className={infoTextStyle}>
-          Do you find it a little difficult to read some of the words above? It
-          can be a little confusing to{" "}
-          <span className="text-red-400 font-bold">read</span> one color, but
-          <span className="text-green-400 font-bold"> see</span> another -- this
-          is known as the{" "}
-          <span className="text-center font-bold">Stroop Effect!</span>
-        </span>
+    <div className={`flex-col text-center w-[350px] px-4 mx-auto`}>
+      <Logo />
+      <div className="flex flex-wrap space-x-2 items-center justify-center my-4">
+        {stroops.map(([label, color], i) => (
+          <Stroop key={i} label={label} color={color} />
+        ))}
+      </div>
+      <div className={infoTextStyle}>
+        Do you find it a little difficult to read some of the words above? It
+        can be a little confusing to{" "}
+        <span className="text-red-400 font-bold">read</span> one color, but
+        <span className="text-green-400 font-bold"> see</span> another -- this
+        is known as the{" "}
+        <span className="text-center font-bold">Stroop Effect!</span>
+      </div>
 
-        <div className="flex-col justify-center items-center my-4">
+      <div className="flex-col justify-center items-center my-4">
+        <span
+          style={colorStyleMap[colorKey]}
+          className="font-bold text-5xl uppercase"
+        >
+          {label}
+        </span>
+        <Grid onClick={onClick} />
+        <div className="font-bold text-5xl uppercase text-slate-100 my-4">
+          {score}
+        </div>
+        <div className={infoTextStyle}>
+          Your goal is to press the{" "}
+          <span style={colorStyleMap[labelColor]}>square</span> that matches the{" "}
+          <span style={colorStyleMap[labelColor]}>written label</span>. When you
+          click the right color, you score points. When you click the wrong
+          color, you lose points!
+        </div>
+        <div className="flex justify-center">
+          <MultiplayerHeader />
+        </div>
+        <div className={infoTextStyle}>
+          You can play Stroopwafel against other people! Either host a game by
+          creating a room or join a friend's game via code.
+        </div>
+        <div className="w-full my-4">
+          <Race
+            goal={MULTIPLAYER_SCORE_TO_WIN}
+            players={[
+              { id: 1, handle: "moop" },
+              { id: 2, handle: "boop" },
+            ]}
+            points={[
+              { userId: 1, val: Math.min(score, MULTIPLAYER_SCORE_TO_WIN) },
+              { userId: 2, val: 6 },
+            ]}
+          />
           <span
             style={colorStyleMap[colorKey]}
-            className="font-bold text-5xl uppercase"
+            className="font-bold text-5xl uppercase my-4 text-center"
           >
             {label}
           </span>
           <Grid onClick={onClick} />
-          <div className="font-bold text-5xl uppercase text-slate-100 my-4">
-            {score}
-          </div>
-          <span className={infoTextStyle}>
-            Your goal is to press the{" "}
-            <span style={colorStyleMap[labelColor]}>square</span> that matches
-            the <span style={colorStyleMap[labelColor]}>written label</span>.
-            When you click the right color, you score points. When you click the
-            wrong color, you lose points!
-          </span>
-          <div className="flex justify-center">
-            <MultiplayerHeader />
-          </div>
-          <span className={infoTextStyle}>
-            You can play Stroopwafel against other people! Either host a game by
-            creating a room or join a friend's game via code.
-          </span>
-          <div className="w-full my-4">
-            <Race
-              goal={MULTIPLAYER_SCORE_TO_WIN}
-              players={[
-                { id: 1, handle: "moop" },
-                { id: 2, handle: "boop" },
-              ]}
-              points={[
-                { userId: 1, val: Math.min(score, MULTIPLAYER_SCORE_TO_WIN) },
-                { userId: 2, val: 6 },
-              ]}
-            />
-            <span
-              style={colorStyleMap[colorKey]}
-              className="font-bold text-5xl uppercase my-4 text-center"
-            >
-              {label}
-            </span>
-            <Grid onClick={onClick} />
-            {score >= MULTIPLAYER_SCORE_TO_WIN && (
-              <div>
-                <div className="text-yellow-400 text-center text-4xl font-bold my-4">
-                  You win!
-                </div>
-                <div
-                  className="py-2 px-4 text-xl text-center text-red-200 hover:cursor-pointer"
-                  onClick={() => setScore(0)}
-                >
-                  Reset
-                </div>
+          {score >= MULTIPLAYER_SCORE_TO_WIN && (
+            <div>
+              <div className="text-yellow-400 text-center text-4xl font-bold my-4">
+                You win!
               </div>
-            )}
-          </div>
-          <span className={infoTextStyle}>
-            When you play against others your in a race to see who can get
-            through all the words the fastest. First to reach the trophy wins!
-          </span>
+              <div
+                className="py-2 px-4 text-xl text-center text-red-200 hover:cursor-pointer"
+                onClick={() => setScore(0)}
+              >
+                Reset
+              </div>
+            </div>
+          )}
         </div>
-        <div className="my-4">Let's play!</div>
+        <div className={infoTextStyle}>
+          When you play against others your in a race to see who can get through
+          all the words the fastest. First to reach the trophy wins!
+        </div>
       </div>
+      <div className="space-x-4 text-left">
+        <a
+          href="https://apps.apple.com/us/app/stroopwafel/id6470153525"
+          className={`${linkTextStyle}`}
+        >
+          Github
+        </a>
+        <a
+          href="https://apps.apple.com/us/app/stroopwafel/id6470153525"
+          className={`${linkTextStyle}`}
+        >
+          App Store
+        </a>
+        <button
+          onClick={() => setPageKey("main")}
+          className={`${linkTextStyle}`}
+        >
+          Main
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Main({ setPageKey }) {
+  return (
+    <div className={`flex-col text-center w-[350px] px-4 mx-auto`}>
+      <Logo />
+      <div className={`${infoTextStyle}`}>
+        Stroopwafel a casual game based on the Stroop effect. The app was built
+        to demonstrate how you can use InstantDB to build and launch real apps
+        to the App Store.
+      </div>
+      <div className={`${infoTextStyle}`}>
+        More than just a toy example, you can see how to leverage Instant to
+        build realtime multiplayer apps.
+      </div>
+      <div className={`${infoTextStyle}`}>
+        We also included a bunch of the detail that comes with shipping mobile
+        apps: from how to navigation in React Native, to deep links, to code
+        organization.
+      </div>
+      <div className="space-x-4 text-left">
+        <a
+          href="https://apps.apple.com/us/app/stroopwafel/id6470153525"
+          className={`${linkTextStyle}`}
+        >
+          Github
+        </a>
+        <a
+          href="https://apps.apple.com/us/app/stroopwafel/id6470153525"
+          className={`${linkTextStyle}`}
+        >
+          App Store
+        </a>
+        <button
+          onClick={() => setPageKey("play")}
+          className={`${linkTextStyle}`}
+        >
+          How to Play
+        </button>
+      </div>
+    </div>
+  );
+}
+
+// App
+// -----------------------------
+function App() {
+  const [pageKey, setPageKey] = useState("main");
+  window.scrollTo(0, 0);
+  let Page;
+  switch (pageKey) {
+    case "main":
+      Page = Main;
+      break;
+    case "play":
+      Page = HowToPlay;
+      break;
+    default:
+      Page = Main;
+      break;
+  }
+  return (
+    <div className={`${bgColor} py-8`}>
+      <Page setPageKey={setPageKey} />
     </div>
   );
 }
